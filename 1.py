84a138250022598286e77e2fabd0ad5785ea770d
soumil0304 <soumilsingh0304@gmail.com>
2026-07-26 15:51:47 +0530

    kettle story

--- 1.py
+++ 1.py
@@ -1,2 +1,3 @@
 sugar_amount = 2 
 print(f"initial amount: {id(sugar_amount)}")
+
---
+++ 2.py
@@ -0,0 +1,3 @@
+masala_spices = ("cardemom","cloves", "cinnamon" )
+spice1, spice2, spice3 = masala_spices
+print(f"main masala spices:{spice1}, {spice2}, {spice3}")
