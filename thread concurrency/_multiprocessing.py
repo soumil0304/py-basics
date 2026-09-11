8e9549ae56061ab58401c5617dda4ac642141285
soumil0304 <soumilsingh0304@gmail.com>
2026-09-11 21:52:17 +0530

    multithreading

---
+++ thread concurrency/_multiprocessing.py
@@ -0,0 +1,24 @@
+from multiprocessing import Process 
+import time 
+
+def brew_chai(name):
+    print(f"{name} chai served ")
+    time.sleep(3)
+    print(f"End of {name} chai brewing")
+    
+if __name__ == "__main__":
+    chai_makers = [
+        Process(target = brew_chai, args = (f"Chai maker #{i+1}",))
+        
+        for i in range(3)
+    ]
+    
+    #start all process 
+    for p in chai_makers:
+        p.start()
+    #wait for  all to complete 
+    for p in chai_makers:
+        p.join()
+        
+    print("All chai served")
+    
